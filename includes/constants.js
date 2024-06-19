46a2ec5b3508139849870278a05d9c78258e1c70
Edgar Alves <edgarsalves@gmail.com>
2024-06-19 14:47:01 +0100

    Add includes files

---
+++ includes/constants.js
@@ -0,0 +1,31 @@
+const projectId = dataform.projectConfig.defaultDatabase;
+const projectRegion = dataform.projectConfig.defaultLocation || "EU";
+
+const defaultDatasetSuffix = dataform.projectConfig.schemaSuffix || "";
+const projectDatasetSuffix = defaultDatasetSuffix ? `_${defaultDatasetSuffix}` : "";
+
+
+const VARS = {
+   PROJECT_ID: `${projectId}`,
+   REGION: `${projectRegion}`,
+   DATASET_SUFFIX: `${projectDatasetSuffix}`,
+
+   //connection defined in the notebook
+   LLM_CONNECTION: "llm_conn",
+
+   LLM_MODEL_BISON: `llm${projectDatasetSuffix}.llm_model_bison`,
+   LLM_MODEL_GEMINI: `llm${projectDatasetSuffix}.llm_model_gemini`,
+
+   //https://console.cloud.google.com/storage/browser/data-analytics-golden-demo/rideshare-lakehouse-raw-bucket/rideshare_audios/v1
+   GCS_URI_SOURCE_REVIEWS_AUDIOS: "gs://data-analytics-golden-demo/rideshare-lakehouse-raw-bucket/rideshare_audios/v1/*.mp3",
+
+   //biglake table to import audios from gcs into bigquery
+   TABLE_SOURCE_REVIEWS_AUDIOS: `source${projectDatasetSuffix}.reviews_audios`,
+
+   //function to extract text from audio
+   EXT_CLOUD_FUNCTION_ENDPOINT: "https://europe-west4-test-gcd-dataform-v1.cloudfunctions.net/bigquery_external_function",
+   EXT_CLOUD_FUNCTION_AI_EXTRACT_TEXT: "extract_text_uri",
+   EXT_UDF_AI_EXTRACT_TEXT: `llm${projectDatasetSuffix}.ext_udf_ai_extract_text`,
+}
+
+module.exports = { VARS }
